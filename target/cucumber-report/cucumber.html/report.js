71d3b207df04216635d93c4207f9b91a1e66b563
kirtana55 <kirtanapatell55@gmail.com>
2023-05-16 21:26:20 +0100

    My Homework week15_1

---
+++ target/cucumber-report/cucumber.html/report.js
@@ -0,0 +1,931 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("ComputerMenu.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Computer menu Test",
+  "description": "",
+  "id": "computer-menu-test",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 5204734800,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 12,
+  "name": "User should add product to shopping cart successfully",
+  "description": "",
+  "id": "computer-menu-test;user-should-add-product-to-shopping-cart-successfully",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 11,
+      "name": "@smoke"
+    },
+    {
+      "line": 11,
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 13,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 14,
+  "name": "I click on Computer Tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 15,
+  "name": "I click on Desktop menu",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 16,
+  "name": "I select Sort By position Name: A to Z",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "I click on Add to Cart of Product Build your own computer",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "I should see Build your own computer",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 19,
+  "name": "I select processor 2.2 GHz Intel Pentium Dual-Core E2200",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "I select ram 8GB [+$60.00]",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "I select HDD 400 GB [+$100.00]",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 22,
+  "name": "I select OS Vista Premium [+$60.00]",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "I click on Microsoft Office [+$50.00]\" and \"Total Commander [+$5.00] checkbox",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 24,
+  "name": "I should see Price $1,475.00",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "I click on ADD TO CART Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "I should see The product has been added to your shopping cart",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 27,
+  "name": "I click on cross button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "I mouse hover on shopping cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "I click on GO TO CART",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "I should see Shopping cart",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 31,
+  "name": "I change Qty of product and update shopping cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 32,
+  "name": "I should see total price is $2,950.00",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 33,
+  "name": "I click on check box of terms and condition",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 34,
+  "name": "I click on checkout button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 35,
+  "name": "I should see Welcome, Please Sign In!",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 36,
+  "name": "I click on CHECKOUT AS GUEST",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 37,
+  "name": "I fill all the mandatory details",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 38,
+  "name": "I click to Continue",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 39,
+  "name": "I select radio button of Next Day Air($0.00)",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 40,
+  "name": "I click on Continue Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 41,
+  "name": "I click on radio button Credit Card",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 42,
+  "name": "I click on Continues",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 43,
+  "name": "I Select Master card From Select credit card dropdown",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 44,
+  "name": "I fill all the card details",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 45,
+  "name": "I click on Continues",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 46,
+  "name": "I should see Payment method is Credit card",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 47,
+  "name": "I should seeShipping method is Next Day Air",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 48,
+  "name": "I should see total price $2,950.00",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 49,
+  "name": "I click on Confirm",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 50,
+  "name": "I should see text Thank You",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 51,
+  "name": "I should see Your order has been successfully processed!",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 52,
+  "name": "I click on Continues button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 53,
+  "name": "I should see Welcome to our store",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TopMenuSteps.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 150522900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnComputerTab()"
+});
+formatter.result({
+  "duration": 1038696900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnDesktopMenu()"
+});
+formatter.result({
+  "duration": 581115500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iSelectSortByPositionNameAToZ()"
+});
+formatter.result({
+  "duration": 220537600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnAddToCartOfProductBuildYourOwnComputer()"
+});
+formatter.result({
+  "duration": 2094315300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iShouldSeeBuildYourOwnComputer()"
+});
+formatter.result({
+  "duration": 1009327000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 19
+    },
+    {
+      "val": "2",
+      "offset": 21
+    },
+    {
+      "val": "2200",
+      "offset": 52
+    }
+  ],
+  "location": "ComputerMenuSteps.iSelectProcessorGHzIntelPentiumDualCoreE(int,int,int)"
+});
+formatter.result({
+  "duration": 218628500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "8",
+      "offset": 13
+    },
+    {
+      "val": "60",
+      "offset": 20
+    },
+    {
+      "val": "00",
+      "offset": 23
+    }
+  ],
+  "location": "ComputerMenuSteps.iSelectRamGB$(int,int,int)"
+});
+formatter.result({
+  "duration": 198080400,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "400",
+      "offset": 13
+    },
+    {
+      "val": "100",
+      "offset": 23
+    },
+    {
+      "val": "00",
+      "offset": 27
+    }
+  ],
+  "location": "ComputerMenuSteps.iSelectHDDGB$(int,int,int)"
+});
+formatter.result({
+  "duration": 151966700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "60",
+      "offset": 29
+    },
+    {
+      "val": "00",
+      "offset": 32
+    }
+  ],
+  "location": "ComputerMenuSteps.iSelectOSVistaPremium$(int,int)"
+});
+formatter.result({
+  "duration": 120850100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "50",
+      "offset": 31
+    },
+    {
+      "val": "00",
+      "offset": 34
+    },
+    {
+      "val": " and ",
+      "offset": 38
+    },
+    {
+      "val": "5",
+      "offset": 63
+    },
+    {
+      "val": "00",
+      "offset": 65
+    }
+  ],
+  "location": "ComputerMenuSteps.iClickOnMicrosoftOffice$TotalCommander$Checkbox(int,int,String,int,int)"
+});
+formatter.result({
+  "duration": 110440700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1",
+      "offset": 20
+    },
+    {
+      "val": "475",
+      "offset": 22
+    },
+    {
+      "val": "00",
+      "offset": 26
+    }
+  ],
+  "location": "ComputerMenuSteps.iShouldSeePrice$(int,int,int)"
+});
+formatter.result({
+  "duration": 2037411800,
+  "error_message": "java.lang.AssertionError: Not displayed expected [$1,470.00] but found [$1,475.00]\r\n\tat org.testng.Assert.fail(Assert.java:96)\r\n\tat org.testng.Assert.failNotEquals(Assert.java:776)\r\n\tat org.testng.Assert.assertEqualsImpl(Assert.java:137)\r\n\tat org.testng.Assert.assertEquals(Assert.java:118)\r\n\tat org.testng.Assert.assertEquals(Assert.java:453)\r\n\tat com.nopcommerce.demo.steps.ComputerMenuSteps.iShouldSeePrice$(ComputerMenuSteps.java:88)\r\n\tat ✽.And I should see Price $1,475.00(ComputerMenu.feature:24)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnADDTOCARTButton()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iShouldSeeTheProductHasBeenAddedToYourShoppingCart()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnCrossButton()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iMouseHoverOnShoppingCart()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnGOTOCART()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iShouldSeeShoppingCart()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iChangeQtyOfProductAndUpdateShoppingCart()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 29
+    },
+    {
+      "val": "950",
+      "offset": 31
+    },
+    {
+      "val": "00",
+      "offset": 35
+    }
+  ],
+  "location": "ComputerMenuSteps.iShouldSeeTotalPriceIs$(int,int,int)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnCheckBoxOfTermsAndCondition()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnCheckoutButton()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iShouldSeeWelcomePleaseSignIn()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnCHECKOUTASGUEST()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iFillAllTheMandatoryDetails()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickToContinue()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "0",
+      "offset": 39
+    },
+    {
+      "val": "00",
+      "offset": 41
+    }
+  ],
+  "location": "ComputerMenuSteps.iSelectRadioButtonOfNextDayAir$(int,int)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnContinueButton()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnRadioButtonCreditCard()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnContinues()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iSelectMasterCardFromSelectCreditCardDropdown()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iFillAllTheCardDetails()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnContinues()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iShouldSeePaymentMethodIsCreditCard()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iShouldSeeShippingMethodIsNextDayAir()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 26
+    },
+    {
+      "val": "950",
+      "offset": 28
+    },
+    {
+      "val": "00",
+      "offset": 32
+    }
+  ],
+  "location": "ComputerMenuSteps.iShouldSeeTotalPrice$(int,int,int)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnConfirm()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iShouldSeeTextThankYou()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iShouldSeeYourOrderHasBeenSuccessfullyProcessed()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iClickOnContinuesButton()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "ComputerMenuSteps.iShouldSeeWelcomeToOurStore()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 1450855600,
+  "status": "passed"
+});
+formatter.uri("Electronic.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Computer menu Test",
+  "description": "",
+  "id": "computer-menu-test",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 3284514900,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 11,
+  "name": "User should add product to Cart and Place the Order Successfully",
+  "description": "",
+  "id": "computer-menu-test;user-should-add-product-to-cart-and-place-the-order-successfully",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 10,
+      "name": "@smoke"
+    },
+    {
+      "line": 10,
+      "name": "@regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 12,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 13,
+  "name": "I mouse hover to Electronics Tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 14,
+  "name": "I mouse hover and click on Cell Phones tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "I should navigate to Cell Phone page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 16,
+  "name": "I click on List view tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 17,
+  "name": "I click on Nokia Lumia 1020",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 18,
+  "name": "I should see Nokia Lumia 1020",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 19,
+  "name": "I should see price $349.00",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "I change the Qty to 2",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "I click on Add to Cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 22,
+  "name": "I should see The product is added to your shopping cart",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 23,
+  "name": "I click cross",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 24,
+  "name": "I mouse hover on Shopping cart and click on Go to Cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "I should see text Shopping cart",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 26,
+  "name": "I should see Qty is 2",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "I should see price is $698.00",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 28,
+  "name": "I click on check box of terms and service",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 29,
+  "name": "I click on Checkout",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 30,
+  "name": "I should see Welcome Please Sign In!",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TopMenuSteps.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 74800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iMouseHoverToElectronicsTab()"
+});
+formatter.result({
+  "duration": 196401600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iMouseHoverAndClickOnCellPhonesTab()"
+});
+formatter.result({
+  "duration": 1020248500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iShouldNavigateToCellPhonePage()"
+});
+formatter.result({
+  "duration": 55436800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iClickOnListViewTab()"
+});
+formatter.result({
+  "duration": 97029500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1020",
+      "offset": 23
+    }
+  ],
+  "location": "ElectronicMenuSteps.iClickOnNokiaLumia(int)"
+});
+formatter.result({
+  "duration": 23007364200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "1020",
+      "offset": 25
+    }
+  ],
+  "location": "ElectronicMenuSteps.iShouldSeeNokiaLumia(int)"
+});
+formatter.result({
+  "duration": 20109872600,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "349",
+      "offset": 20
+    },
+    {
+      "val": "00",
+      "offset": 24
+    }
+  ],
+  "location": "ElectronicMenuSteps.iShouldSeePrice$(int,int)"
+});
+formatter.result({
+  "duration": 39474900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 20
+    }
+  ],
+  "location": "ElectronicMenuSteps.iChangeTheQtyTo(int)"
+});
+formatter.result({
+  "duration": 244873400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iClickOnAddToCart()"
+});
+formatter.result({
+  "duration": 86855800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iShouldSeeTheProductIsAddedToYourShoppingCart()"
+});
+formatter.result({
+  "duration": 290147700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iClickCross()"
+});
+formatter.result({
+  "duration": 95759400,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iMouseHoverOnShoppingCartAndClickOnGoToCart()"
+});
+formatter.result({
+  "duration": 1624797900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iShouldSeeTextShoppingCart()"
+});
+formatter.result({
+  "duration": 56331700,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "2",
+      "offset": 20
+    }
+  ],
+  "location": "ElectronicMenuSteps.iShouldSeeQtyIs(int)"
+});
+formatter.result({
+  "duration": 88421100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "698",
+      "offset": 23
+    },
+    {
+      "val": "00",
+      "offset": 27
+    }
+  ],
+  "location": "ElectronicMenuSteps.iShouldSeePriceIs$(int,int)"
+});
+formatter.result({
+  "duration": 49291700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iClickOnCheckBoxOfTermsAndService()"
+});
+formatter.result({
+  "duration": 104676300,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iClickOnCheckout()"
+});
+formatter.result({
+  "duration": 549447900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "ElectronicMenuSteps.iShouldSeeWelcomePleaseSignIn()"
+});
+formatter.result({
+  "duration": 134821900,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1017523000,
+  "status": "passed"
+});
+});
